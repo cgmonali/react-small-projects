fec783cb86c71d1e05181c107170b8b497615373
Monali C G <cgmonali@gmail.com>
2023-06-25 09:43:14 +0530

    first react sample project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+
+
+
+function App() {
+  return (
+    <div>
+      
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+// a component is just a special kind of function which return html code
+
+function ExpenseItems(){
+    return <div>
+        <h4>Expense Item</h4>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movies Rs 200</p>
+    </div>
+}
+
+
+
+export default ExpenseItems;
