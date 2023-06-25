0d11bb261e4127d4263d59e875a1eb6ec0d2c4a0
Monali C G <cgmonali@gmail.com>
2023-06-25 10:41:31 +0530

    added css file and location of expense variable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
 // a component is just a special kind of function which return html code
 
+import "./ExpenseItem.css"
+
 function ExpenseItems(){
-    return <div>
-        <h4>Expense Item</h4>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
+    const expenseDate=new Date(2021, 2,28);
+    const expenseTitle="car insurance";
+    const expenseAmount=294.67;
+    const LocationOfExpenditure="New Delhi";
+    return <div className="expense-item ">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+        </div>
     </div>
 }
 
